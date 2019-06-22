5745f56ea3909ca7368b1fa55c71c191852ac124
kazu <jddpd083@gmail.com>
2019-06-22 06:26:25 +0000

    9. 2. 2 表示に対応しよう

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190622044152) do
+ActiveRecord::Schema.define(version: 20190622060356) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -21,8 +21,8 @@ ActiveRecord::Schema.define(version: 20190622044152) do
     t.string "remember_digest"
     t.boolean "admin", default: false
     t.string "department"
-    t.time "basic_time"
-    t.datetime "work_time"
+    t.datetime "basic_time", default: "2019-06-21 23:00:00"
+    t.datetime "work_time", default: "2019-06-21 22:30:00"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
