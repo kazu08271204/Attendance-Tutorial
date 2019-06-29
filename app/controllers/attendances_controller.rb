527ec8f1d632b26f3308307794842f3f532da6e7
kazu <jddpd083@gmail.com>
2019-06-29 03:01:58 +0000

    10. 7 出勤日数を表示しよう

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -1,5 +1,7 @@
 class AttendancesController < ApplicationController
   
+  UPDATE_ERROR_MSG = "勤怠登録に失敗しました。やり直してください。"
+  
   def update
   @user = User.find(params[:user_id])
   @attendance = Attendance.find(params[:id])
@@ -9,9 +11,17 @@ class AttendancesController < ApplicationController
     if @attendance.update_attributes(started_at: Time.current.change(sec: 0))
     flash[:info] = "おはようございます！"
     else
-    flash[:danger] = "勤怠登録に失敗しました。やり直したてください。"
+    flash[:danger] = UPDATE_ERROR_MSG
     end
+  elsif @attendance.finished_at.nil?
+    if @attendance.update_attributes(finished_at: Time.current.change(sec: 0))
+      flash[:info] = "お疲れさまでした。"
+    else
+      flash[:denger] = UPDATE_ERROR_MSG
+    end
+    
   end
+  
   redirect_to @user
   end
 end
